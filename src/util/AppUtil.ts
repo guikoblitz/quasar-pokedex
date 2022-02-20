fc0a6a25bad7f24541650c8eb7d9316decc21009
guikoblitz <guilhermerk_contato@hotmail.com>
2022-02-20 20:59:17 -0300

    feat: new apputil functions and consts

--- src/util/AppUtil.ts
+++ src/util/AppUtil.ts
@@ -6,3 +6,8 @@ export function getPokemonIdByUrlString(url: string) {
   const urlList = url.split('/');
   return Number(urlList[urlList.length - 2]);
 }
+
+export function returnPokemonId(pokemonId: number): string {
+  const pokemonIdString = String(pokemonId);
+  return pokemonIdString.length >= 3 ? `#${pokemonId}` : pokemonIdString.length === 2 ? `#0${pokemonId}` : `#00${pokemonId}`;
+}
--- src/util/Constantes.ts
+++ src/util/Constantes.ts
@@ -20,3 +20,24 @@ export const POKEMON_TYPES = {
   POISON: '#A33EA1',
   PSYCHIC: '#F95587',
 } as PokemonTypeColor;
+
+export const POKEMON_TYPES_CARD = {
+  ROCK: '#C1B474',
+  STEEL: '#B7b7CE', // original
+  WATER: '#ACC2F2',
+  BUG: '#B9C657',
+  DARK: '#705746', // original
+  DRAGON: '#A481FD',
+  ELECTRIC: '#F9DF77',
+  FAIRY: '#D685AD',
+  FIGHTING: '#CC6B68',
+  FIRE: '#F0AC78',
+  FLYING: '#A98FF3', // opção: E1D9F4
+  GHOST: '#735797', // opção: D4C8E3
+  GRASS: '#A6D08E',
+  GROUND: '#E8D7AE',
+  ICE: '#96D9D6', // original
+  NORMAL: '#A8A77A', // original
+  POISON: '#A33EA1', // opção: F95587
+  PSYCHIC: '#FBA0BC',
+} as PokemonTypeColor;
